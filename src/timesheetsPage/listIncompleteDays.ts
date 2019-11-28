c76f698486313921ea31a0fd4ada58d7e075099d
jkanchelov <jkanchelov@gmail.com>
2019-11-28 11:59:36 +0200

    Add return type

--- src/timesheetsPage/listIncompleteDays.ts
+++ src/timesheetsPage/listIncompleteDays.ts
@@ -1,6 +1,6 @@
 import pageSelectors from "../enums/selectors";
 import { Page } from "puppeteer";
-export default async (page: Page) => {
+export default async (page: Page): Promise<string[]> => {
     let list = await page.evaluate(
         selectors =>
             Array.from(document.querySelectorAll(selectors.dayLists)[0].children).map((el: Element) => {
@@ -8,5 +8,6 @@ export default async (page: Page) => {
             }),
         pageSelectors
     );
+
     return list;
 };
